bc7fcbf9bdc2cbd4026946ed88805f210a87a8bb
Luci Sousa <marialuci148@gmail.com>
2022-09-10 20:35:55 -0300

    commit da classe Pessoa

--- 3_conhecendoPlataforma.NETcomC#/Fundamentos_testes/Models/Pessoa.cs
+++ 3_conhecendoPlataforma.NETcomC#/Fundamentos_testes/Models/Pessoa.cs
@@ -2,6 +2,16 @@ namespace Fundamentos_testes.Models
 {
     public class Pessoa
     {
-        
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+
+        public void Apresentar()
+        {
+
+            System.Console.WriteLine($"Ola, meu nome é {Nome} e tenho {Idade} anos");
+        }
+
+
     }
 }
