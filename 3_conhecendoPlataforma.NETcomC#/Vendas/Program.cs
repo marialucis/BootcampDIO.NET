2a8d459fbe0b12747b6c9318b04ae3bb4dd84341
Luci Sousa <marialuci148@gmail.com>
2022-09-12 23:57:55 -0300

    Testando operadores

---
+++ 3_conhecendoPlataforma.NETcomC#/Vendas/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+
+int estoqueQuantidade = 100;
+int estoqueCompra = 0;
+bool possivelVenda =  estoqueQuantidade >= estoqueCompra;
+
+Console.WriteLine("Digite a quantidade a comprar: ");
+estoqueCompra = int.Parse(Console.ReadLine());
+
+if (estoqueQuantidade >= estoqueCompra)
+{
+    Console.WriteLine("Venda Realizada");
+}
+else
+{
+    Console.WriteLine("Desculpe, sem estoque");
+}
+
