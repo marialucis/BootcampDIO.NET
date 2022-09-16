2e732351f3c25c0fc678acde4d38a4d69048d007
Luci Sousa <marialuci148@gmail.com>
2022-09-16 12:49:17 -0300

    Subindo projeto Tabuada para testar o Laço de repeticao FOR

---
+++ 3_conhecendoPlataforma.NETcomC#/Tabuada/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite um numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+for (int contador = 0; contador < 11 ; contador++)
+{
+    Console.WriteLine($"{numero} * {contador} = {numero * contador}");
+}
