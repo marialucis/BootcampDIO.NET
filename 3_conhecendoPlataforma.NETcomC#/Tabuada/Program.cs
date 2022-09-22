9d0501a6ae2129382e5bbcee13037048e541789d
Luci Sousa <marialuci148@gmail.com>
2022-09-22 15:42:16 -0300

    Atualizando repositorio

--- 3_conhecendoPlataforma.NETcomC#/Tabuada/Program.cs
+++ 3_conhecendoPlataforma.NETcomC#/Tabuada/Program.cs
@@ -1,9 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Digite um numero: ");
-int numero = int.Parse(Console.ReadLine());
+//usando while
 
-for (int contador = 0; contador < 11 ; contador++)
+int numero = 5;
+int contador = 0;
+
+while (contador <= 10)
 {
     Console.WriteLine($"{numero} * {contador} = {numero * contador}");
+    contador++;
+   
+    
+    if (contador == 11)
+    {
+        break; //qubra o laco
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //usando for
+// Console.WriteLine("Digite um numero: ");
+// int numero = int.Parse(Console.ReadLine());
+
+// for (int contador = 0; contador < 11 ; contador++)
+// {
+//     Console.WriteLine($"{numero} * {contador} = {numero * contador}");
+// }
