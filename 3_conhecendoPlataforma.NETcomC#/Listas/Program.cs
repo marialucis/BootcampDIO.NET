a184d4958a2bc82e3b41afcd96ab2c9cf27898e7
Luci Sousa <marialuci148@gmail.com>
2022-09-26 23:08:49 -0300

    Testando listas e arrays

---
+++ 3_conhecendoPlataforma.NETcomC#/Listas/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Testando Listas!");
+
+//declarando a lista
+List<string> lista = new List<string>(); 
+lista.Add("AM");
+lista.Add("PB");
+lista.Add("PE");
+
+foreach (string item in lista)
+{
+    Console.WriteLine(item);
+}
