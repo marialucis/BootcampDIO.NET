2c4de2bc04dbf36ab32c9cd79de77a20361598d4
Luci Sousa <marialuci148@gmail.com>
2022-09-30 11:44:34 -0300

    Subindo projeto de testes com praticas em propriedades, metodos e construtores

---
+++ 3_conhecendoPlataforma.NETcomC#/Fundamentos_part2/Models/Pessoa.cs
@@ -0,0 +1,58 @@
+namespace Fundamentos_part2.Models
+{
+    public class Pessoa
+    {
+        //criando campo para utilizar a propriedade
+        private string _nome;
+        private int _idade;
+        //propriedades
+        public string Nome
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome está vazio"); //criando excecao
+                }
+
+                _nome = value;
+            }
+
+        }
+        public int Idade 
+        { 
+            get => _idade;
+        
+         set
+         {
+            if (value < 0)
+            {
+            throw new ArgumentException("A idade está vazio");
+            }
+
+            _idade = value;
+
+         }
+         
+          }
+
+
+        //metodos
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ 3_conhecendoPlataforma.NETcomC#/Fundamentos_part2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Fundamentos_part2.Models;
+{
+    
+    //instanciando a classe e suas propriedades
+    Pessoa pessoa = new Pessoa();
+
+    pessoa.Nome = "Teste" ;
+    pessoa.Idade = -1 ;
+
+
+    pessoa.Apresentar();
+    
+
+
+
+
+}
