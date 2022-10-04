2d6bc2413e688557ab82fccbd39c2cd74f1f7773
Luci Sousa <marialuci148@gmail.com>
2022-10-04 00:42:34 -0300

    Subindo implementaçãos dos metodos adicionar, remover, listar e obter alunos da classe curso. Também foi implementado a opção de Menu na classe principal para escolher os metodos

--- 3_conhecendoPlataforma.NETcomC#/Fundamentos_part2/Models/Curso.cs
+++ 3_conhecendoPlataforma.NETcomC#/Fundamentos_part2/Models/Curso.cs
@@ -7,15 +7,36 @@ namespace Fundamentos_part2.Models
 
         public List<Pessoa> Alunos { get; set; }
 
+       
 
 
-        public void AdicionarAluno(Pessoa aluno)
+
+        public void AdicionarAluno(Pessoa aluno) //adiciona alunos a coleção de lista
         {
             Alunos.Add(aluno);
         }
         
-            
 
+        public void RemoverAluno(Pessoa aluno)
+        {
+
+            Alunos.Remove(aluno);
+        }
+
+
+        public void ListarAlunos()
+        {
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+
+        public int ObterAlunosMatriculados()
+        {
+            int quantidades = Alunos.Count(); //metodo Count usando para obter a quantidade de alunos
+            return quantidades;
+        }
 
     }
 }
--- 3_conhecendoPlataforma.NETcomC#/Fundamentos_part2/Program.cs
+++ 3_conhecendoPlataforma.NETcomC#/Fundamentos_part2/Program.cs
@@ -2,28 +2,59 @@
 using Fundamentos_part2.Models;
 {
     //variaveis
-    bool exibeMenu = true;
     int opc = 0;
+    bool exibeMenu = true;
+    
 
     //instanciando a classe e suas propriedades
-    //Pessoa pessoa = new Pessoa();
+    Pessoa pessoa = new Pessoa();
 
    // pessoa.Nome = "Teste" ;
     //pessoa.Idade = 12 ;
-
-
     //pessoa.Apresentar();
     
+    //instanciando a clsse Curso
+    Curso curso = new Curso();
 
     while (exibeMenu)
     {
-        Console.WriteLine("Escolha a opção");
-        Console.WriteLine("1- Cadastrar Aluno");
+        
+        Console.WriteLine("1- Adicionar Aluno");
         Console.WriteLine("2- Remover Aluno");
         Console.WriteLine("3- Listar Alunos");
+        Console.WriteLine("4- Obter Alunos Matriculados");
         Console.WriteLine("0- SAIR");
+        Console.WriteLine("Escolha a opção");
+        opc = int.Parse(Console.ReadLine());
+
+        
+        switch (opc)
+        {   
+            // case 1:
+            //     curso.AdicionarAluno();     
+            // break;
+            
+            // case 2:
+            //     curso.RemoverAluno();     
+            // break;
+
+            case 3:
+                curso.ListarAlunos();     
+            break;
+            
+            case 4:
+                curso.ObterAlunosMatriculados();     
+            break;
+            
+            
+            default:
+                Console.WriteLine("Opção Invalida");
+                exibeMenu = false;
+                Environment.Exit(0);
+            break;
+        }
 
     }
-
+        Console.ReadLine();
 
 }
