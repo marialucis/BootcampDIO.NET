c08d11e946ef2fa3da4bb62b2b3111b92f62eb3d
Luci Sousa <marialuci148@gmail.com>
2022-10-06 18:04:19 -0300

    Subindo projeto com praticas de concatenacao e interpolacao de STRING

---
+++ 3_conhecendoPlataforma.NETcomC#/ConcatenandoStringInterpolacao/ConcatenandoStringInterpolacao/ConcatenandoStringInterpolacao/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcatenandoStringInterpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Nome = " ";
+            string SobreNome = " ";
+
+            Console.WriteLine("Digite o seu Nome: ");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Digite o seu Sobrenome: ");
+            SobreNome = Console.ReadLine();
+
+
+            string NomeCompleto = Nome + SobreNome;
+
+            ////concatenando string
+            Console.WriteLine("Nome Completo: " +NomeCompleto);
+
+
+
+            //interpolacao
+
+            Console.WriteLine($"Nome Completo: {Nome} {SobreNome}");
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
