d8b40d4a73eb56c102352ed23c7dcaee19252ff2
Luci Sousa <marialuci148@gmail.com>
2022-10-10 23:58:27 -0300

    Subindo projeto com praticas if ternario

---
+++ 3_conhecendoPlataforma.NETcomC#/IFternario/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// //declarando variaveis
+ bool ehpar = false;
+
+
+ Console.WriteLine("Digite um numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+
+//if ternario
+
+ehpar = numero % 2 == 0;
+Console.WriteLine($"O numero {numero} é " +(ehpar ? "par" : "impar"));
+
+
+
+
+System.Console.WriteLine("Aperte enter para encerrar...");
+Console.ReadLine();
