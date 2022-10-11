dbc36a4a3b32073d98f7c5803ca5613f1d51a6a7
Luci Sousa <marialuci148@gmail.com>
2022-10-11 00:04:45 -0300

    Subindo projeto praticas com tuplas

---
+++ 3_conhecendoPlataforma.NETcomC#/TrabalhandoTuplas/TuplasOpeTernario/TuplasOpeTernario/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuplasOpeTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            (int, string, string, decimal) tupla = (1, "Lux", "Teste", 1.52M);  //testando tupla(declarando os tipos de dados atraves de uma unica variavel tupla)
+            Console.WriteLine($"ID: {tupla.Item1}");
+            Console.WriteLine($"Nome: {tupla.Item2}");
+            Console.WriteLine($"Sobrenome: {tupla.Item3}");
+            Console.WriteLine($"Altura: {tupla.Item4}");
+
+            Console.WriteLine("\n");
+
+            //outra forma de declarar tuplas
+            //usando o ValueTuple
+
+            ValueTuple<int, string, string, decimal> outroExemploTupla = (2, "Luxi", "Testes", 1.52M);
+
+            Console.WriteLine($"ID: {outroExemploTupla.Item1}");
+            Console.WriteLine($"Nome: {outroExemploTupla.Item2}");
+            Console.WriteLine($"Sobrenome: {outroExemploTupla.Item3}");
+            Console.WriteLine($"Altura: {outroExemploTupla.Item4}");
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+        
+    }
+}
