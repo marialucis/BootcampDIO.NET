0465e09c03ec778fd4b3a0b6bc76dea804be855f
Luci Sousa <marialuci148@gmail.com>
2022-10-23 14:20:30 -0300

    Subindo desafio sistema hotel

---
+++ 3_conhecendoPlataforma.NETcomC#/DesafiosC#/trilha-net-explorando-desafio-main/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Luci", "Sousa");
+Pessoa p2 = new Pessoa(nome: "Teste 2", "Ex");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+// Cria a suíte
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 65);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 20);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
