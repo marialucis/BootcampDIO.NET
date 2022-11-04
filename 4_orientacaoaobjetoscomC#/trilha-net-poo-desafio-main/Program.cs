c2409d30e7d4faf0fb66f80d2c920070b6fd0661
Luci Sousa <marialuci148@gmail.com>
2022-11-03 23:26:43 -0300

    Subindo projeto Sistema abstração de Celular

--- 4_orientacaoaobjetoscomC#/trilha-net-poo-desafio-main/Program.cs
+++ 4_orientacaoaobjetoscomC#/trilha-net-poo-desafio-main/Program.cs
@@ -1,3 +1,29 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//instanciando a classe nokia
+Nokia nokia = new Nokia();
+Console.WriteLine($"O numero é: {nokia.Numero ="523819"}");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
+//instanciando iphone
+
+Iphone iphone = new Iphone();
+Console.WriteLine($"O numero é: {iphone.Numero = "10101010"}");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tinder");
+
+
+
+
+
+
+
+Console.WriteLine("Aperte qualquer tecla para encerrar...");
+Console.ReadLine();
